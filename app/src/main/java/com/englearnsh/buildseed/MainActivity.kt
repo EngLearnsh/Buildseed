a41958badb111ca9247cb257163f6b6ca4a3c97d
EngLearnsh <cxlanyagege@hotmail.com>
2020-08-05 19:32:46 +0800

    Add drawer and toolbar

--- app/src/main/java/com/englearnsh/buildseed/MainActivity.kt
+++ app/src/main/java/com/englearnsh/buildseed/MainActivity.kt
@@ -1,27 +1,29 @@
 package com.englearnsh.buildseed
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.os.StrictMode
-import android.util.Log
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.MutableState
+import androidx.compose.state
 import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
+import androidx.ui.foundation.*
 import androidx.ui.layout.Column
+import androidx.ui.layout.fillMaxSize
+import androidx.ui.layout.fillMaxWidth
 import androidx.ui.layout.padding
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.Surface
+import androidx.ui.material.*
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.Menu
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
+
 import com.englearnsh.buildseed.ui.BuildseedTheme
 import com.englearnsh.buildseed.util.HtmlParse
 
 class MainActivity : AppCompatActivity() {
-
-    val TAG = "Buildseed"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,29 +35,122 @@ class MainActivity : AppCompatActivity() {
         }
         setContent {
             BuildseedTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting()
-                }
+                AppComponent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting() {
-    val version = HtmlParse().parseVersion()
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text(text = "$version")
+fun AppComponent() {
+    val (drawerState, onDrawerStateChange) = state { DrawerState.Closed }
+    val currentScreen = state { AppScreen.Home }
+
+    ModalDrawerLayout(
+            drawerState = drawerState,
+            onStateChange = onDrawerStateChange,
+            gesturesEnabled = drawerState == DrawerState.Opened,
+            drawerContent = {
+                DrawerContentComponent(
+                        currentScreen = currentScreen,
+                        closeDrawer = { onDrawerStateChange(DrawerState.Closed)
+                        }
+                )
+            },
+            bodyContent = {
+                BodyContentComponent(
+                        currentScreen = currentScreen.value,
+                        openDrawer = {
+                            onDrawerStateChange(DrawerState.Opened)
+                        }
+                )
+            }
+    )
+}
+
+@Composable
+fun DrawerContentComponent(
+        currentScreen: MutableState<AppScreen>,
+        closeDrawer: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        for (index in AppScreen.values().indices) {
+            val screen = getScreenBasedOnIndex(index)
+            Box(Modifier.clickable(onClick = {
+                currentScreen.value = screen
+                closeDrawer()
+            }), children = {
+                Surface(
+                        modifier = Modifier.fillMaxWidth(),
+                        color = if (currentScreen.value == screen) {
+                            MaterialTheme.colors.secondary
+                        } else {
+                            MaterialTheme.colors.surface
+                        }
+                ) {
+                    Text(text = screen.name, modifier = Modifier.padding(16.dp))
+                }
+            })
+        }
     }
 }
 
+/**
+ * Return the corresponding screen composable based on user interactive index
+ */
+fun getScreenBasedOnIndex(index: Int) = when (index) {
+    0 -> AppScreen.Home
+    else -> AppScreen.Home
+}
+
+/**
+ * Pass the corresponding screen composable based on current active screen
+ */
+@Composable
+fun BodyContentComponent(
+        currentScreen: AppScreen,
+        openDrawer: () -> Unit
+) {
+    when (currentScreen) {
+        AppScreen.Home -> HomeComponent(
+                openDrawer
+        )
+    }
+}
+
+@Composable
+fun HomeComponent(openDrawer: () -> Unit) {
+
+    val version = HtmlParse().parseVersion()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar(
+                title = { Text(text = "Buildseed") },
+                navigationIcon = {
+                    IconButton(onClick = openDrawer) {
+                        Icon(asset = Icons.Filled.Menu)
+                    }
+                }
+        )
+        Surface(color = MaterialTheme.colors.background, modifier = Modifier.weight(1f)) {
+            Box(modifier = Modifier.padding(16.dp), gravity = ContentGravity.Center, children = {
+                Text(text = "$version")
+            })
+        }
+    }
+}
+
+enum class AppScreen {
+    Home
+}
+
+/**
+ * Composable preview directly inside Android Studio
+ */
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BuildseedTheme {
-        Greeting()
+        AppComponent()
     }
 }
